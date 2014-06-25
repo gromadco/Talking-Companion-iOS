caa551fea710e3caa0140d0ec4ac9572501e56ff
Sergey Butenko <sergeyzp2010@gmail.com>
2014-06-25 10:08:47 +0300

    Missed file was removed

--- Talking Companion/Talking Companion-Bridging-Header.h.h
+++
@@ -1,9 +0,0 @@
-//
-//  Talking Companion-Bridging-Header.h.h
-//  Talking Companion
-//
-//  Created by Sergey Butenko on 24.06.14.
-//  Copyright (c) 2014 serejahh inc. All rights reserved.
-//
-
-#import "SMXMLDocument.h"
