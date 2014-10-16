1adf3495117043f96adc9ef7fb6d5bc16d6f7ca5
Artem Dudarev <dudarev@gmail.com>
2014-10-16 23:44:17 +0300

    got map features for en and ru in csv file - #31

--- scripts/get_osm_features.py
+++ scripts/get_osm_features.py
@@ -2,12 +2,13 @@
 Parse OpenStreetMap map features.
 """
 
+import csv
 from lxml import html
-from lxml.etree import tostring
 import os
+
 import requests
 
-article_title = 'Map_Features'
+article_title = 'RU:Map_Features'
 file_name = article_title + '.html'
 article_url = 'http://wiki.openstreetmap.org/wiki/' + article_title
 
@@ -24,21 +25,19 @@ tree = html.fromstring(text)
 trs = tree.xpath('//tr')
 print len(trs)
 
+csv_writer = csv.writer(open('map_features.csv', 'w'))
+
 for tr in trs:
     tds = tr.xpath('.//td')
     if len(tds) > 3:
         key = tds[0]
+        key = ''.join(key.xpath('.//text()')).strip()
         value = tds[1]
-        element = tds[2]
+        value = value.text_content().strip().encode('utf8')
         comment = tds[3]
-        print 'key=', ''.join(key.xpath('.//text()')).strip()
-
-# for tr in trs:
-#     print td
-#     td_text = td.text_content().strip()
-#     if td_text:
-#         print td_text
-#     else:
-#         print td.xpath('//a')[0].text_content()
-
-print dir(tr)
+        comment = comment.text_content().strip().encode('utf8')
+        print '--------'
+        print 'key=', key
+        print 'value=', value
+        print 'comment=', comment
+        csv_writer.writerow((key, value, comment))
